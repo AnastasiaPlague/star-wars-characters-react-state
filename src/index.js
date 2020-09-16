959913e281cd8127e43835792a028bad62ad7090
Anastasia <great-swindler@mail.ru>
2020-09-16 22:47:11 +0300

    Implement a custom hook for fetching data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -6,11 +6,36 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import CharacterList from './CharacterList';
 
 import dummyData from './dummy-data';
-
+import endpoint from './endpoint';
 import './styles.scss';
 
+const useFetch = (url) => {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setResponse([]);
+    setError(null);
+
+    fetch(url + '/characters')
+      .then((response) => response.json())
+      .then((response) => {
+        setLoading(false);
+        setResponse(response);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(error);
+      });
+  }, []);
+  return [response, loading, error];
+};
+
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [response, loading, error] = useFetch(endpoint + '/characters');
+  const chachters = (response && response.characters) || [];
 
   return (
     <div className="Application">
@@ -19,7 +44,8 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? <p>Loading</p> : <CharacterList characters={characters} />}
+          {error && <p className="error">{error.message}</p>}
         </section>
       </main>
     </div>
